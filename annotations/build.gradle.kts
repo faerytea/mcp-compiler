31be13f595928de19168748a459a38ca25ea72a8
faerytea <faerytea@gmail.com>
2026-04-30 20:33:52 +0600

    Add maven publishing

--- annotations/build.gradle.kts
+++ annotations/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm")
+    alias(libs.plugins.maven.publish)
 }
 
 group = "name.faerytea.mcp.annotations"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
 
 kotlin {
     jvmToolchain(8)
@@ -11,4 +12,38 @@ kotlin {
 
 dependencies {
     testImplementation(kotlin("test"))
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    coordinates(group.toString(), name, version.toString())
+
+    pom {
+        name.set("MCP Compiler annotations")
+        description.set("Annotations for MCP tools compiler.")
+        inceptionYear.set("2026")
+        url.set("https://github.com/faerytea/mcp-compiler/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("faerytea")
+                name.set("Valery Maevsky")
+                url.set("https://faerytea.name")
+            }
+        }
+        scm {
+            url.set("https://github.com/faerytea/mcp-compiler/")
+            connection.set("scm:git:git://github.com/faerytea/mcp-compiler.git")
+            developerConnection.set("scm:git:ssh://git@github.com/faerytea/mcp-compiler.git")
+        }
+    }
 }
--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm")
+    alias(libs.plugins.maven.publish)
 }
 
 group = "name.faerytea.mcp.compiler"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
 
 dependencies {
     implementation(project(":annotations"))
@@ -11,4 +12,38 @@ dependencies {
     implementation(libs.square.kotlin.poet.ksp)
     implementation(libs.ksp.api)
     testImplementation(kotlin("test"))
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    coordinates(group.toString(), name, version.toString())
+
+    pom {
+        name.set("MCP Compiler KSP processor")
+        description.set("KSP processor for generating builerplate wrappers for MCP tools.")
+        inceptionYear.set("2026")
+        url.set("https://github.com/faerytea/mcp-compiler/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("faerytea")
+                name.set("Valery Maevsky")
+                url.set("https://faerytea.name")
+            }
+        }
+        scm {
+            url.set("https://github.com/faerytea/mcp-compiler/")
+            connection.set("scm:git:git://github.com/faerytea/mcp-compiler.git")
+            developerConnection.set("scm:git:ssh://git@github.com/faerytea/mcp-compiler.git")
+        }
+    }
 }
